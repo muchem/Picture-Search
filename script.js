bf3c3497b0ecf2934996b6bbbe1c5cd1f45312e4
muchemm <melissauchem@gmail.com>
2019-10-09 18:49:17 -0700

    make AJAX call function

---
+++ script.js
@@ -0,0 +1,10 @@
+const getImages = () => {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+}
