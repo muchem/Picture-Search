c63d9f5221a364e99da2be06de339f08db422c89
muchemm <melissauchem@gmail.com>
2019-10-09 19:37:27 -0700

    replace ajax call with async function and add a input value check function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const getImages = () => {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send();
+let results = document.getElementById('results');
+let data = "";
+let responce;
+let Url = "https://pixabay.com/api/";
+let ApiKey = "?key=13875463-5946a46e49401d6e5a6de2d2b";
+let searchTxt;
+document.getElementById("search-bnt").disabled = true;
+const checkValue = () => {
+    if (document.getElementById('searchBar').value !== "") {
+        document.getElementById("search-bnt").disabled = false;
+    }
+}
+const getImages = async () => {
+    searchTxt = document.getElementById('searchBar').value;
+    const api = `${Url}${ApiKey}&q=${searchTxt}&image_type=photo`;
+    const responce = await fetch(api);
+    const data = await responce.json();
+    console.log(data);
+    searchTxt = "";
 }
